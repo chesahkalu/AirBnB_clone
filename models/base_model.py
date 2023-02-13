7a05135c0c95b135243b66c1e0befe7c2bf7709d
chesahkalu <chesahkalu@gmail.com>
2023-02-13 23:26:14 +0000

    Basemodel created to initialise some instance attribute and include some serializtion/diserialization method

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""contains the base model class that defines
+all common attributes/methods for other classes:"""
+
+import models
+import uuid
+from datetime import datetime
+
+time_format = "%Y-%m-%dT%H:%M:%S.%f"
+
+class Basemodel:
+        """contains the base model class that defines
+        all common attributes/methods for other classes:"""
+
+        def __init__(self, id , created_at , updated_at):
+            """using *args and **kwargs to create an instance with a passed
+            dictionary values"""
+
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+	def __str__(self):
+              """String printable representation of Basemodel instance"""
+
+              return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        
+        def to_dict(self):
+              """Returns dictionary format of all instances attribute,
+              class name, created_at and updated_at made readable as it
+              is in a data structure."""
+
+              all_dict = self.__dict__.copy()
+              all_dict["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+              all_dict["updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+              all_dict["__class__"] = self.__class__.__name__
+              return all_dict
+        
+        def save(self):
+              """Updates the attribute updated at with the current date and time"""
+
+              self.updated_at = datetime.now()
