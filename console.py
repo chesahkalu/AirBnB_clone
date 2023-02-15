b97758a14e60e2f51992ec3b19984dfa7871b498
chesahkalu <chesahkalu@gmail.com>
2023-02-15 09:46:11 +0000

    fix

--- console.py
+++ console.py
@@ -37,7 +37,7 @@ class HBNBCommand(cmd.Cmd):
         """Creates a new instance of a class"""
         args = shlex.split(arg)
         if len(args) == 0:
-            print("** class name missing **")
+           print("** class name missing **")
             return False
         if args[0] in classes:
             instance = classes[args[0]]()
